449208350e60057079caa91e7d104c2f9ef30ae0
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-05-04 00:06:16 -0300

    Fix: add Runwith

--- src/test/java/com/trumands/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/trumands/springjenkins/SpringJenkinsApplicationTests.java
@@ -3,18 +3,22 @@ package com.trumands.springjenkins;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing");
-		assertEquals(true, true);
+		assertTrue(true);
 	}
 
 }
